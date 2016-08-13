9d20ce4dde35cadbd43500aacfbcb2a6bfd936d5
josecefe <josec@192.168.0.9>
2016-08-13 05:01:24 +0200

    Primera versión "usable" sin completar la edición básica

--- matlab/minqdist.m
+++
@@ -1,4 +0,0 @@
-function [ dist ] = minqdist( vec, matriz )
-%MINQDIST Calcula la distancia cuadratica menor del vector al conjunto dado
-    dist = min(sum((repmat(vec, size(matriz,1), 1) - matriz) .^ 2, 2));
-end
