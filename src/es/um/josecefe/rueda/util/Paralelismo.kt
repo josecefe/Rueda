14758ffb181d355a8054bc249ee5d1fd2a464221
josecefe <josecefe@gmail.com>
2017-09-26 10:40:33 +0200

    Introduciendo paralelismo mediante funciones genericas sobre las colecciones con corutinas

--- src/es/um/josecefe/rueda/util/Paralelismo.kt
+++ src/es/um/josecefe/rueda/util/Paralelismo.kt
@@ -8,37 +8,18 @@
 
 package es.um.josecefe.rueda.util
 
+import kotlinx.coroutines.experimental.CommonPool
+import kotlinx.coroutines.experimental.async
+import kotlinx.coroutines.experimental.runBlocking
 import java.util.*
 import java.util.concurrent.ExecutorService
 import java.util.concurrent.Executors
 import java.util.concurrent.TimeUnit
 
-inline fun <T, R> Iterable<T>.pmap(
-        numThreads: Int = Runtime.getRuntime().availableProcessors() - 2,
-        exec: ExecutorService = Executors.newFixedThreadPool(numThreads),
-        crossinline transform: (T) -> R): List<R> {
-
-    // default size is just an inlined version of kotlin.collections.collectionSizeOrDefault
-    val defaultSize = if (this is Collection<*>) this.size else 10
-    val destination = Collections.synchronizedList(ArrayList<R>(defaultSize))
-
-    for (item in this) {
-        exec.submit { destination.add(transform(item)) }
-    }
-
-    exec.shutdown()
-    exec.awaitTermination(1, TimeUnit.DAYS)
-
-    return ArrayList<R>(destination)
+fun <A, B> List<A>.pmap(f: suspend (A) -> B): List<B> = runBlocking {
+    map { async(CommonPool) { f(it) } }.map { it.await() }
 }
 
-inline fun <T> Iterable<T>.pforEach(
-        numThreads: Int = Runtime.getRuntime().availableProcessors() - 2,
-        exec: ExecutorService = Executors.newFixedThreadPool(numThreads),
-        crossinline transform: (T) -> Unit): Unit {
-
-    for (item in this) exec.submit { transform(item) }
-
-    exec.shutdown()
-    exec.awaitTermination(1, TimeUnit.DAYS)
+fun <A> Collection<A>.forEachParallel(f: suspend (A) -> Unit): Unit = runBlocking {
+    map { async(CommonPool) { f(it) } }.forEach { it.await() }
 }
